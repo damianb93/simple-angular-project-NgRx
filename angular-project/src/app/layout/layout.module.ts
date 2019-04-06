c89ab6446488c4be5e25bf68e58159e1acab6396
damianb93 <damianburak93@gmail.com>
2019-04-06 14:05:56 +0200

    feat: Implement header component

--- angular-project/src/app/layout/layout.module.ts
+++ angular-project/src/app/layout/layout.module.ts
@@ -3,6 +3,7 @@ import {ContentLayoutComponent} from "./content-layout/content-layout.component"
 import {CommonModule} from "@angular/common";
 import {RouterModule} from "@angular/router";
 import {HeaderComponent} from "./header/header.component";
+import {NgbCollapseModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import {HeaderComponent} from "./header/header.component";
   ],
   imports: [
     CommonModule,
+    NgbCollapseModule,
     RouterModule.forChild([])
   ]
 })
